3e78ba822afea68673ed460d4b0b5daea294843c
lsoobin <vini20012203@gmail.com>
2025-07-22 10:13:52 +0900

    fix: admin.py 최신순 정렬

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at')  # 목록에서 보여줄 필드
+    list_display_links = ('title',)               # 클릭 가능한 필드
+    search_fields = ('title', 'context')          # 검색 가능 필드
+    list_filter = ('created_at',)                 # 필터 사이드바
+    ordering = ('-created_at',)                   # 최신순 정렬
